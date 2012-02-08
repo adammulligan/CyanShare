587c7720642a7b486312253c06bcf2968b3d4142
root <root@cyanoryx.com>
2012-02-08 17:03:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyanshare::Application.config.secret_token = '52d73e68fedce3070db9f0fa481fb26e46ef7353b5279b25a646a3103b2afd12835d1db373143b91c7a9b4cba8dbc91c70d2941d1c2c17f9b14b686cc166227e'
